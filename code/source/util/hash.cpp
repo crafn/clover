c69716c5d4e5f359d0d89ba775e63ffc770453a1
crafn <tuukka.pensala@gmail.com>
2014-12-01 15:04:31 +0200

    Reduced hash collisions

--- code/source/util/hash.cpp
+++ code/source/util/hash.cpp
@@ -5,10 +5,10 @@ namespace clover {
 namespace util {
 
 uint32 byteHash32(const uint8* buf, SizeType size){
-	// FNV-1a
+	// Modified FNV-1a
 	uint32 hash= 2166136261;
 	for (SizeType i= 0; i < size; ++i){
-		hash= (hash ^ buf[i])*16777619;
+		hash= ((hash ^ buf[i]) + 379721)*16777619;
 	}
 	return hash;
 }
@@ -16,7 +16,7 @@ uint32 byteHash32(const uint8* buf, SizeType size){
 uint32 Hash32<real32>::operator()(real32 value) const {
 	union { real32 f; uint32 i; } u;
 	u.f= value;
-	return hash32(u.i);
+	return u.i;
 }
 
 uint32 Hash32<real64>::operator()(real64 value) const {
