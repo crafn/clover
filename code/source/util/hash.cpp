24cb11600fce5ac35ecb142a7e8b4d69a5c78e04
crafn <tuukka.pensala@gmail.com>
2014-12-08 23:45:43 +0200

    Duff's device seems to be helpful for hash function

--- code/source/util/hash.cpp
+++ code/source/util/hash.cpp
@@ -5,11 +5,35 @@ namespace clover {
 namespace util {
 
 uint32 byteHash32(const uint8* buf, SizeType size){
+#define USE_DUFF_OPTIMZ 1
+	PROFILE();
+
 	// Modified FNV-1a
 	uint32 hash= 2166136261;
+
+#if USE_DUFF_OPTIMZ
+#define HASH \
+	do { hash= ((hash ^ *buf++) + 379721)*16777619; } while (0)
+	if (size == 0)
+		return hash;
+	SizeType n= (size + 7)/8;
+	switch (size % 8) {
+		case 0: do { HASH;
+		case 7: HASH;
+		case 6: HASH;
+		case 5: HASH;
+		case 4: HASH;
+		case 3: HASH;
+		case 2: HASH;
+		case 1: HASH; } while (--n > 0);
+	}	
+#undef HASH
+#else
 	for (SizeType i= 0; i < size; ++i){
 		hash= ((hash ^ buf[i]) + 379721)*16777619;
 	}
+#endif
+
 	return hash;
 }
 
