c0450b82988cfa831e927eddadf3d77ce71b24a2
crafn <tuukka.pensala@gmail.com>
2014-12-28 20:23:58 +0200

    RealMultiply from script to code

---
+++ code/source/nodes/native_instances/nodeinstance_multiply_real.hpp
@@ -0,0 +1,31 @@
+#ifndef CLOVER_NODES_NODEINSTANCE_MULTIPLY_REAL_HPP
+#define CLOVER_NODES_NODEINSTANCE_MULTIPLY_REAL_HPP
+
+#include "../nodeinstance.hpp"
+#include "build.hpp"
+
+namespace clover {
+namespace nodes {
+
+class RealMultiplyNodeInstance : public NodeInstance {
+public:
+	virtual void create() override;
+
+private:
+	InputSlot<SignalType::Real>* input1;
+	InputSlot<SignalType::Real>* input2;
+	OutputSlot<SignalType::Real>* realOut;
+};
+
+} // nodes
+namespace util {
+
+template <>
+struct TypeStringTraits<nodes::RealMultiplyNodeInstance> {
+	static util::Str8 type(){ return "::RealMultiplyNodeInstance"; }
+};
+
+} // util
+} // clover
+
+#endif // CLOVER_NODES_NODEINSTANCE_MULTIPLY_REAL_HPP
